d55fbb64a4413489977fff88c816eba32be303b3
Tewesday <39603201+NoahKinslow@users.noreply.github.com>
2022-01-14 13:04:14 -0600

    add behavior for modify_git_config_user

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::io;
+use std::process::Command;
 
 fn main() {
     let name = input_user();
-    modify_git_config_user(name);
+    modify_git_config_user(&name);
 }
 
 fn input_user() -> String {
@@ -15,6 +16,26 @@ fn input_user() -> String {
     name.to_string()
 }
 
-fn modify_git_config_user(username: String) {
-    
+fn modify_git_config_user(username: &String) {
+    let change_username_command = Command::new("git")
+        .args(["config", "--global", "user.name", username])
+        .output() // execute command
+        .expect("process to change user.name failed");
+
+    if change_username_command.status.success() {
+        println!("process to change user.name success");
+    } else {
+        println!("result: {}", change_username_command.status);
+    }
+
+    let change_credential_command = Command::new("git")
+        .args(["config", "--global", "credential.username", username])
+        .output() // execute command
+        .expect("process to change credential.username failed");
+
+    if change_credential_command.status.success() {
+        println!("process to change credential.username success");
+    } else {
+        println!("result: {}", change_credential_command.status);
+    }
 }

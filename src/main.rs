d5befe66e08aac0621831ff929be7a21bb2fc189
Tewesday <39603201+NoahKinslow@users.noreply.github.com>
2022-01-14 12:39:22 -0600

    add io for getting username from user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let name = input_user();
+    modify_git_config_user(name);
+}
+
+fn input_user() -> String {
+    println!("Input desired username");
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).expect("Name invalid");
+
+    let name = name.trim();
+
+    name.to_string()
+}
+
+fn modify_git_config_user(username: String) {
+    
 }
